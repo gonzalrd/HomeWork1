2a5b32bc444ef17bb5285d3fdaf2a5375a90d556
Nicholas Uth <uthnp@muohio.edu>
2012-09-06 19:05:13 -0400

    Code Review Edits Made
    General tweaks here and there. Cut out an unnecessary object or two out.
    Fixed a small error with the color coding on the tint that was mixing up
    the green value and the blue value.

--- src/HomeWork1App.cpp
+++ src/HomeWork1App.cpp
@@ -25,7 +25,7 @@ class HomeWork1App : public AppBasic {
   private:
 	Surface* mySurface_; //The Surface object whose pixel array we will modify
   
-	//Track how many frames we have shown, for animatino purposes
+	//Track how many frames we have shown, for animation purposes
 	int frame_number_;
 	boost::posix_time::ptime app_start_time_;
 
@@ -36,7 +36,7 @@ class HomeWork1App : public AppBasic {
 
 	/** This satisfies the "rectangle" requirement, goal A.1
 	 */
-	void drawRectangles(uint8_t* pixels, int x1, int y1,  int rect_width, int rect_height, Color8u fill);
+	void drawRectangles(uint8_t* pixels, int x1, int y1,  int rect_width, int rect_height, Color8u c);
 	/** Satisfies the circle requiremtn goal A.2 
 	*/
 	void drawCircle(uint8_t* pixels, int center_x, int center_y, int r, Color8u c);
@@ -59,13 +59,9 @@ void HomeWork1App::prepareSettings(Settings* settings){
 	(*settings).setResizable(false);
 }
 
-//Draws the rectangles for the rectangle requirment.
-void HomeWork1App::drawRectangles(uint8_t* pixels, int x1, int y1, int rect_width, int rect_height, Color8u fill){
-
-	Color8u c = Color8u(192,123,0);
-	c.r = fill.r;
-	c.b = fill.b;
-	c.g = fill.g;
+//Draws the rectangles for the rectangle requirement.
+void HomeWork1App::drawRectangles(uint8_t* pixels, int x1, int y1, int rect_width, int rect_height, Color8u c){
+	// cut out the object intermediate since you can just access the color directly
 
 	for(int x = x1; x<rect_height; x++){
 		for(int y = y1; y<rect_width; y++){
@@ -77,12 +73,13 @@ void HomeWork1App::drawRectangles(uint8_t* pixels, int x1, int y1, int rect_widt
 	}
 }
 
+//Draws the circles for the circle requirement.
 void HomeWork1App::drawCircle(uint8_t* pixels, int center_x, int center_y, int r, Color8u c){
 	for(int y=center_y-r; y<=center_y+r; y++){
 		for(int x=center_x-r; x<=center_x+r; x++){
 			//Bounds test, to make sure we don't access array out of bounds
 			if(y < 0 || x < 0 || x >= kAppWidth || y >= kAppHeight) continue;
-
+			// ^that's a nice idea that I wish I had thought to implement XD;;
 					
 			int dist = (int)sqrt((double)((x-center_x)*(x-center_x) + (y-center_y)*(y-center_y)));
 			if(dist <= r){
@@ -95,6 +92,7 @@ void HomeWork1App::drawCircle(uint8_t* pixels, int center_x, int center_y, int r
 	}
 }
 
+//Creates the gradient for the gradient requirement.
 void HomeWork1App::createGradient(){
 		//Satisfies requirment gradient requirement A4
 	if(gradColor.r<255) gradColor.r = gradColor.r + 10;
@@ -106,24 +104,32 @@ void HomeWork1App::createGradient(){
 }
 //Takes in the array of pixels and sets the blue color in each one to change the color
 void HomeWork1App::tint(uint8_t* pixels){
-
-	Color8u c = Color8u(0,100,0);
+	// an off-by-one error was mixing up the g and b values. the function should be working as intended now.
+	Color8u c = Color8u(0,0,100); // was originally setting b = 0. 
 	for(int col = 0; col<800; col++){
 		for(int row = 0;  row<600; row++){
-			pixels[3*(col + row*kTextureSize)+1] = c.b;
+			pixels[3*(col + row*kTextureSize)+2] = c.b;
 		}
 	}
 }
 
+//applies the blur for the blur requirement.
 void HomeWork1App::blur(uint8_t* pixels){
-	for(int col = 2; col<799; col++){
-		for(int row = 2;  row<599; row++){
-
-				pixels[3*(col + row*kTextureSize)] = (pixels[3*(col + row*kTextureSize)] + pixels[3*((col+1) + row*kTextureSize)] + pixels[3*((col+1) + (row+1)*kTextureSize)] + pixels[3*((col) + (row+1)*kTextureSize)] + pixels[3*((col-1) + (row+1)*kTextureSize)] + pixels[3*((col-1) + (row-1)*kTextureSize)] + pixels[3*((col) + (row-1)*kTextureSize)])/9;
-				pixels[3*(col + row*kTextureSize)+1] = (pixels[3*(col + row*kTextureSize)+1] + pixels[3*((col+1) + row*kTextureSize)+1] + pixels[3*((col+1) + (row+1)*kTextureSize)+1] + pixels[3*((col) + (row+1)*kTextureSize)+1] + pixels[3*((col-1) + (row+1)*kTextureSize)+1] + pixels[3*((col-1) + (row-1)*kTextureSize)+1] + pixels[3*((col) + (row-1)*kTextureSize)+1])/9;
-				pixels[3*(col + row*kTextureSize)+2] = (pixels[3*(col + row*kTextureSize)+2] + pixels[3*((col+1) + row*kTextureSize)+2] + pixels[3*((col+1) + (row+1)*kTextureSize)+2] + pixels[3*((col) + (row+1)*kTextureSize)+2] + pixels[3*((col-1) + (row+1)*kTextureSize)+2] + pixels[3*((col-1) + (row-1)*kTextureSize)+2] + pixels[3*((col) + (row-1)*kTextureSize)+2])/9;
-					
+	// changed it so that the size of the frame could be changed without any problems if someone wanted
+	for(int col = 2; col<(kAppWidth-1); col++){
+		for(int row = 2;  row<(kAppHeight-1); row++){
 	
+				for (int i = 0; i < 3; i++){ 
+					// put the math in one for loop for readability
+					// also spaced out the equation so it can all be seen at once.
+					pixels[3*(col + row*kTextureSize)+i] = (pixels[3*(col + row*kTextureSize)+i] + 
+						pixels[3*((col+1) + row*kTextureSize)+i] + 
+						pixels[3*((col+1) + (row+1)*kTextureSize)+i] + 
+						pixels[3*((col) + (row+1)*kTextureSize)+i] + 
+						pixels[3*((col-1) + (row+1)*kTextureSize)+i] + 
+						pixels[3*((col-1) + (row-1)*kTextureSize)+i] + 
+						pixels[3*((col) + (row-1)*kTextureSize)+i])/9;
+				}
 		}
 	}
 }
@@ -144,12 +150,10 @@ void HomeWork1App::mouseDown( MouseEvent event )
 	//Get our array of pixel information
 	uint8_t* dataArray = (*mySurface_).getData();
 	blur(dataArray);
-	
 }
 
 void HomeWork1App::update()
 {
-	
 	//Get our array of pixel information
 	uint8_t* dataArray = (*mySurface_).getData();
 
@@ -179,21 +183,18 @@ void HomeWork1App::update()
 	int circleX = 10;
 	int circleY = 60;
 	while(circleX<800){
-	createGradient();
-	drawCircle(dataArray, circleX, 20, 10, gradColor);
-	drawCircle(dataArray, circleX, 570, 10, gradColor);
-	circleX = circleX + 40;
-		}
+		createGradient();
+		drawCircle(dataArray, circleX, 20, 10, gradColor);
+		drawCircle(dataArray, circleX, 570, 10, gradColor);
+		circleX = circleX + 40;
+	}
 	while(circleY<560){
-	createGradient();
-	drawCircle(dataArray, 10, circleY, 10, gradColor);
-	drawCircle(dataArray, 770, circleY, 10, gradColor);
-	circleY = circleY + 40;
-		}
+		createGradient();
+		drawCircle(dataArray, 10, circleY, 10, gradColor);
+		drawCircle(dataArray, 770, circleY, 10, gradColor);
+		circleY = circleY + 40;
+	}
 	
-	
-	
-
  //Only save the first frame of drawing as output
 	if(frame_number_ == 0){
 		writeImage("raquelImage.png",*mySurface_);
